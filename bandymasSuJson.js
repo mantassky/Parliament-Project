76404248f8d5e2e76c2d0327e1eeb8a65dec6a85
mantassky <96481000+mantassky@users.noreply.github.com>
2023-02-24 15:31:02 +0100

    trying to make it work

--- bandymasSuJson.js
+++ bandymasSuJson.js
@@ -2,7 +2,7 @@
 
 window.onload = () => {
     const getFiles = async () => {
-        let response = await fetch("https://raw.githubusercontent.com/mantassky/SeimasTempFiles/main/masterObj.json")
+        let response = await fetch("https://raw.githubusercontent.com/mantassky/Parliament-Project/main/masterObj.json")
         master = await response.json()
         response = await fetch("https://raw.githubusercontent.com/mantassky/Parliament-Project/main/objFrakcijos.json")
         objFrakcijos = await response.json()
